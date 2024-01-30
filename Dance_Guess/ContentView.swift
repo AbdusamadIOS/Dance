834dc3afe884e9a8fab9b5828cb35a3fbe011e36
abdusamad <abdusamad2609@gmail.com>
2024-01-30 17:59:38 +0500

    second commit

--- Dance_Guess/ContentView.swift
+++ Dance_Guess/ContentView.swift
@@ -9,78 +9,58 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var isBlueColor = false
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        ZStack{
+            Color(isBlueColor ? .blue : .red)
+                .animation(Animation.easeInOut(duration:
+                                                0.2).repeatForever(autoreverses: false), value: isBlueColor)
+                .onAppear {
+                    isBlueColor.toggle()
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+                .ignoresSafeArea()
+            AnimationSequenceView()
+                .offset(x: 12, y: 180)
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
+}
+
+var images: [UIImage]! = [
+    UIImage(named: "0")!,
+    UIImage(named: "1")!,
+    UIImage(named: "2")!,
+    UIImage(named: "3")!,
+    UIImage(named: "4")!,
+    UIImage(named: "5")!,
+    UIImage(named: "6")!,
+    UIImage(named: "7")!,
+    UIImage(named: "8")!,
+    UIImage(named: "9")!,
+    UIImage(named: "10")!,
+    UIImage(named: "11")!,
+   
+]
+let animatedImages = UIImage.animatedImage(with: images, duration: 0.8)
+
+struct AnimationSequenceView : UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> some UIView {
+        
+        let sequenceView = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+        let imageView = UIImageView (frame: sequenceView.bounds)
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = animatedImages
+        
+        sequenceView.addSubview(imageView)
+        return sequenceView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
 }
